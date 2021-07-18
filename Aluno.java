ff4ed156c479aba6dc38de8bf7dd799f1d72e33f
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-07-18 19:42:04 -0300

    Desafio do filter

--- Aluno.java
+++ Aluno.java
@@ -3,9 +3,11 @@ package streamapi;
 public class Aluno {
     final String nome;
     final double nota;
+    final boolean eEstudiosa;
 
-    public Aluno(String nome, double nota) {
+    public Aluno(String nome, double nota, boolean eEstudiosa) {
         this.nome = nome;
         this.nota = nota;
+        this.eEstudiosa = eEstudiosa;
     }
 }
--- Filter.java
+++ Filter.java
@@ -2,21 +2,28 @@ package streamapi;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Filter {
     public static void main(String[] args) {
 
-        Aluno aluno1 = new Aluno("Ana", 7.1);
-        Aluno aluno2 = new Aluno("Maria", 7.8);
-        Aluno aluno3 = new Aluno("Helena", 5.8);
-        Aluno aluno4 = new Aluno("Bianca", 9.8);
-        Aluno aluno5 = new Aluno("Bruna", 6.8);
-        Aluno aluno6 = new Aluno("Eduarda", 8.8);
+        Aluno aluno1 = new Aluno("Ana", 7.1, false);
+        Aluno aluno2 = new Aluno("Maria", 7.8, false);
+        Aluno aluno3 = new Aluno("Helena", 5.8, true);
+        Aluno aluno4 = new Aluno("Bianca", 9.8, false);
+        Aluno aluno5 = new Aluno("Bruna", 6.8, true);
+        Aluno aluno6 = new Aluno("Eduarda", 8.8, true);
 
         List<Aluno> alunos = Arrays.asList(aluno1, aluno2, aluno3, aluno4, aluno5, aluno6);
+
+        Predicate<Aluno> aprovado = aluno -> aluno.nota >= 7 && aluno.eEstudiosa;
+        Function<Aluno, String> saudacaoAprovado =
+                aluno -> "Parabens " + aluno.nome + "! você foi aprovada!";
+
         alunos.stream()
-                .filter(aluno -> aluno.nota >= 7)
-                .map(aluno -> "Parabės " + aluno.nome + "! você foi aprovada! ")
+                .filter(aprovado)
+                .map(saudacaoAprovado)
                 .forEach(System.out::println);
     }
 }
