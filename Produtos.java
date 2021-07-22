f6372ee63e9e18e52c2070530d25f900666fc4bd
Thalita Ribeiro <sribeiro.thalita@gmail.com>
2021-07-21 22:58:10 -0300

    Desafio Filter

---
+++ Produtos.java
@@ -0,0 +1,15 @@
+package streamapi;
+
+public class Produtos {
+    final String nome;
+    final double preco;
+    final double desconto;
+    final double valorFrete;
+
+    public Produtos(String nome, double preco, double desconto, double valorFrete) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+        this.valorFrete = valorFrete;
+    }
+}
